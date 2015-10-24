f275baf9b4ecf44ce1c34ff392b015cce1fdd080
Cheyan <cheyan.set@gmail.com>
2015-10-24 14:32:03 -0400

    fixing post

--- public/javascripts/submitparty.js
+++ public/javascripts/submitparty.js
@@ -5,17 +5,16 @@ $("#submit").click(function() {
             async: false,
             type: "POST",
             data: {
-               date: getDate(),
-               phone: $('#telephone').val(),
-               email: $('#email').val(),
-               address: $('#address').val(),
-               coordinates: {
-                 latitude: location[0],
-                 longitude: location[1]},
-               complaints: [{
-                 date: null,
-                 note: null
-               }]
+              party: {
+                date: getDate(),
+                phone: $('#telephone').val(),
+                email: $('#email').val(),
+                address: $('#address').val(),
+                coordinates: {
+                  latitude: location[0],
+                  longitude: location[1]
+                },
+              },
             },
 
             contentType: "application/x-www-form-urlencoded", //This is what made the difference.
@@ -29,6 +28,6 @@ function getDate() {
   var dateTime = new Date($(".datepicker").datepicker("getDate"));
   var strDateTime =  dateTime.getDate() + "/" + (dateTime.getMonth()+1) + "/" + dateTime.getFullYear();
   console.log(strDateTime);
-
+  
   return strDateTime;
 }
