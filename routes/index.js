5b2d900da62ba821013835a0494911fc77205789
justincoon <justin.tyler.coon@gmail.com>
2015-10-24 14:24:02 -0400

    adding favicon

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var db = require('../js/db.js');
+var sms = require('twilio')('AC5de47b0bf29da6a9b89a08f35d2c3877', '8a0c2fb532eaab6c58bda9665a2558a2'); 
 
 router.get('/', function(req, res, next) {
 	res.render('index');
@@ -23,6 +24,16 @@ router.post('/party', function(req, res, next) {
 router.post('/parties', function(req, res, next) {
 	db.getParties(req.body.coordinates, .25 , function(response) {
 		res.send(response);
+	});
+});
+
+router.post('/complain', function(req, res, next) {
+	sms.messages.create({     
+		body: req.body.complaint.note,
+		to: "+1 413-537-2758",
+		from: "+1 413-650-1988" 
+	}, function(err, message) { 
+		console.log(message.sid); 
 	});
 });
 module.exports = router;
