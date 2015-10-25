72f3fcd21d2cb70b267c2fa2f1d4c66e94b10d83
Cheyan <cheyan.set@gmail.com>
2015-10-25 01:12:01 -0400

    calendar limit, required fields

--- public/javascripts/submitparty.js
+++ public/javascripts/submitparty.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   $('.modal-trigger').leanModal();
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 1,
+    min: true
+  });
 });
 
 $("#submit").click(function() {
